e6cae9f362216db83421befe3dcd50cd0bf3ffc4
Aman Maurya <amanmaurya8661@gmail.com>
2026-02-18 00:12:46 +0530

    simplify clip-manager flow

--- dot-config/hypr/scripts/clip-manager.sh
+++ dot-config/hypr/scripts/clip-manager.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
-## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# Clipboard Manager. This script uses cliphist, rofi, and wl-copy.
+
+#   ____ _ _       _     _     _
+#  / ___| (_)_ __ | |__ (_)___| |_
+# | |   | | | '_ \| '_ \| / __| __|
+# | |___| | | |_) | | | | \__ \ |_
+#  \____|_|_| .__/|_| |_|_|___/\__|
+#           |_|
+#
 
 # Actions:
 # CTRL Del to delete an entry
 # ALT Del to wipe clipboard contents
 
 while true; do
-    result=$(
-        rofi -dmenu \
-            -kb-custom-1 "Control-Delete" \
-            -kb-custom-2 "Alt-Delete" \
-            -config ~/.config/rofi/themes/clipboard.rasi < <(cliphist list)
+    result=$( cliphist list | rofi -dmenu \
+                -kb-custom-1 "Control-Delete" \
+                -kb-custom-2 "Alt-Delete" \
+                -config ~/.config/rofi/config-cliphist.rasi
     )
 
     case "$?" in
-        1)
+        1)  # 1 - Escape
             exit
             ;;
-        0)
-            case "$result" in
-                "")
-                    continue
-                    ;;
-                *)
-                    cliphist decode <<<"$result" | wl-copy
-                    exit
-                    ;;
-            esac
+        0)  # 0 - Normal enter
+            [ -z "$result" ] && continue
+            cliphist decode <<< "$result" | wl-copy
+            exit
             ;;
-        10)
+        10) # custom keys start at exit code 10
             cliphist delete <<<"$result"
             ;;
         11)
